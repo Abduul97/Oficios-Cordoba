1b87e436bb979981d9a6652001893faa3dc00447
Abdul Emgert <abdulska97@gmail.com>
2026-01-01 19:59:51 -0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ['oficios-cordoba-production.up.railway.app']
+  }
 })
